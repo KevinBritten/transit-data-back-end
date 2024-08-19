64e58b1d26ce1fd38f523fa837921978daf89bb6
Kevin Britten <kevinjbritten@gmail.com>
2024-08-19 19:11:58 -0400

    add checkToken function

---
+++ checkToken/index.js
@@ -0,0 +1,69 @@
+module.exports = async function (context, req) {
+  const sql = require("mssql");
+
+  // Handle preflight OPTIONS request
+  if (req.method === "OPTIONS") {
+    context.res = {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      },
+      status: 204,
+      body: "",
+    };
+    return;
+  }
+
+  const config = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER,
+    database: process.env.DB_DATABASE,
+    port: parseInt(process.env.DB_PORT),
+    options: {
+      encrypt: process.env.DB_TRUST_CERTIFICATE === "true", // Logically check for string 'true'
+      trustServerCertificate: process.env.DB_ENCRYPT === "true", // Logically check for string 'true'
+    },
+    requestTimeout: 15000,
+  };
+
+  let data = req.body; // Use real data in production
+
+  try {
+    console.log("Attempting to connect to the database...");
+    let pool = await sql.connect(config);
+    console.log("Connected to the database successfully.");
+
+    let result = await pool
+      .request()
+      .input("accessToken", sql.VarChar, data.accessToken)
+      .output("status", sql.Int)
+      .output("message", sql.VarChar)
+      .execute("checkToken");
+
+    console.log(result.recordset[0]);
+
+    console.log("Query executed successfully:", result);
+    context.res = {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      status: 200,
+      body: {
+        message: result.output.message,
+        status: result.output.status,
+        recordset: result.recordset[0],
+      },
+    };
+  } catch (err) {
+    console.error("SQL error", err);
+    context.res = {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      status: 500,
+      body: JSON.stringify({ error: "Server Error" }),
+    };
+  }
+};
